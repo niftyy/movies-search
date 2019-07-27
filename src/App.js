635fa53a7767ef6a80eb91903a56fd82047c321e
niftyy <tusharsparte@gmail.com>
2019-07-27 12:16:20 +0530

    added api, finished functioning project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React from "react";
 import "./App.css";
 import MovieRow from "./MovieRow";
+import $ from "jquery";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    console.log("this is a initializer");
-
-    const movies = [
-      { id: 0, title: "Avengers: Infinity War" },
-      { id: 1, title: "Avengers: EndGame" }
-    ];
-
-    var movieRows = [];
-    movies.forEach(movie => {
-      console.log(movie.title);
-      const movieRow = <MovieRow movie={movie} />;
-      movieRows.push(movieRow);
-    });
-
-    this.state = {
-      rows: movieRows
-    };
+    this.state = {};
+    //this.performSearch(searchTerm);
   }
+
+  performSearch(searchTerm) {
+    const urlString =
+      "https://api.themoviedb.org/3/search/movie?&api_key=da17eed40cc1258d79d206c8a72880dc&language=en-US&page=1&include_adult=false&query=" +
+      searchTerm;
+    $.ajax({
+      url: urlString,
+      success: searchResults => {
+        console.log("Fetched data successfully");
+
+        const results = searchResults.results;
+        console.log(results[0]);
+        let movieRows = [];
+
+        results.forEach(movie => {
+          //console.log(movie.title);
+          const movieRow = <MovieRow key={movie.id} movie={movie} />;
+          movieRows.push(movieRow);
+        });
+
+        this.setState({
+          rows: movieRows
+        });
+      },
+      error: (xhr, status, err) => {
+        console.error("Failed to fetch data");
+      }
+    });
+    console.log("MoviesDB Search Performed");
+  }
+
+  searchChangeHandler(event) {
+    console.log(event.target.value);
+    const searchTerm = event.target.value;
+    this.performSearch(searchTerm);
+  }
+
   render() {
     return (
       <div className="App">
@@ -50,6 +73,7 @@ class App extends React.Component {
             paddingLeft: 16
           }}
           placeholder="Enter search term"
+          onChange={this.searchChangeHandler.bind(this)}
         />
         {this.state.rows}
       </div>
--- src/MovieRow.js
+++ src/MovieRow.js
@@ -1,18 +1,36 @@
 import React from "react";
 
 class MovieRow extends React.Component {
+  viewMovie() {
+    const url = "https://www.themoviedb.org/movie/" + this.props.movie.id;
+    window.location.href = url;
+  }
+
   render() {
     return (
       <table key={this.props.movie.id}>
         <tbody>
           <tr>
             <td>
-              <img alt="poster" width="120" src="" />
+              <img
+                alt="poster"
+                width="120"
+                src={
+                  "http://image.tmdb.org/t/p/w185/" +
+                  this.props.movie.poster_path
+                }
+              />
             </td>
             <td>
-              {this.props.movie.title}
+              <h3>{this.props.movie.title}</h3>
               <p>{this.props.movie.overview}</p>
+              <input
+                type="button"
+                onClick={this.viewMovie.bind(this)}
+                value="View"
+              />
             </td>
+            <td />
           </tr>
         </tbody>
       </table>
